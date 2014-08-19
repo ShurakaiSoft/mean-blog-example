c87c7a606bb1b2c4a0bce0f35f3e085f771a399b
Stephen Parker <shurakaisoft@gmail.com>
2014-08-19 21:44:49 +1000

    updates

---
+++ test/controllerSpec.js
@@ -0,0 +1,18 @@
+/**
+ * Test unit test.
+ */
+
+
+
+describe("Controllers", function () {
+	beforeEach(module('blogDemo'));
+
+	describe("IndexCtrl", function () {
+		beforeEach(inject(function () {
+
+		}));
+        it("should work", function () {
+            expect(false).toEqual(false);
+        });
+	});
+});
